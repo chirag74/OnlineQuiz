cb6c7cf8752d0eeae8995db7daea3098094c60a2
chirag74 <chiragkumar31081999@gmail.com>
2022-08-08 16:56:43 +0530

    uicommit

---
+++ src/app/pages/user/start/start.component.ts
@@ -0,0 +1,134 @@
+import { LocationStrategy } from '@angular/common';
+import { ThisReceiver } from '@angular/compiler';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { QuestionService } from 'src/app/services/question.service';
+import { QuizService } from 'src/app/services/quiz.service';
+import Swal from 'sweetalert2';
+declare var google:any;
+@Component({
+  selector: 'app-start',
+  templateUrl: './start.component.html',
+  styleUrls: ['./start.component.css'],
+})
+export class StartComponent implements OnInit {
+  qid;
+  questions;
+
+  marksGot = 0;
+  correctAnswers = 0;
+  attempted = 0;
+
+  isSubmit = false;
+
+  timer: any;
+quizzes:[];
+totalmarks=0;
+  constructor(
+    private locationSt: LocationStrategy,
+    private _route: ActivatedRoute,
+    private _question: QuestionService,
+    private _quiz: QuizService
+  ) {}
+
+  ngOnInit(): void {
+    this.preventBackButton();
+    this.qid = this._route.snapshot.params.qid;
+    console.log(this.qid);
+    this.loadQuestions();
+
+    
+
+    this._quiz.quizzes().subscribe(
+      (data: any) => {
+        this.quizzes = data;
+        console.log(this.quizzes);
+      },
+      (error) => {
+        console.log(error);
+        Swal.fire('Error !', 'Error in loading data !', 'error');
+      }
+    );
+ 
+  }
+ 
+ 
+
+
+  
+  loadQuestions() {
+    this._question.getQuestionsOfQuizForTest(this.qid).subscribe(
+      (data: any) => {
+        this.questions = data;
+
+        this.timer = this.questions.length * 2 * 60;
+
+        console.log(this.questions);
+        this.startTimer();
+      },
+
+      (error) => {
+        console.log(error);
+        
+      }
+    );
+  }
+
+  preventBackButton() {
+    history.pushState(null, null, location.href);
+    this.locationSt.onPopState(() => {
+      history.pushState(null, null, location.href);
+    });
+  }
+
+  submitQuiz() {
+    Swal.fire({
+      title: 'Do you want to submit the quiz?',
+      showCancelButton: true,
+      confirmButtonText: `Submit`,
+      icon: 'info',
+    }).then((e) => {
+      if (e.isConfirmed) {
+        this.evalQuiz();
+      }
+    });
+  }
+
+  startTimer() {
+    let t = window.setInterval(() => {
+      if (this.timer <= 0) {
+        this.evalQuiz();
+        clearInterval(t);
+      } else {
+        this.timer--;
+      }
+    }, 1000);
+  }
+
+  getFormattedTime() {
+    let mm = Math.floor(this.timer / 60);
+    let ss = this.timer - mm * 60;
+    return `${mm} min : ${ss} sec`;
+  }
+
+  evalQuiz() {
+    
+    this._question.evalQuiz(this.questions).subscribe(
+      (data: any) => {
+        console.log(data);
+        this.marksGot = data.marksGot;
+        this.correctAnswers = data.correctAnswers;
+        this.attempted = data.attempted;
+        this.isSubmit = true;
+      },
+      (error) => {
+        
+      }
+    );
+
+  }
+
+// getanswer(){
+//   this.totalmarks=this.quizzes.maxMarks;
+// }
+}
